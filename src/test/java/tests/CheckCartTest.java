46fe107d9d00f843a88d2f713f3e6b8eee92bd03
Lidiya_Bogatyrenko <lidia.bogatyrenko@gmail.com>
2026-04-23 01:05:54 +0300

    SauceDemo HW1 Задание1: Локаторы + Задание2: Корзина. Убрала пустые строки, добавила комменты.

--- src/test/java/tests/CheckCartTest.java
+++ src/test/java/tests/CheckCartTest.java
@@ -1,3 +1,5 @@
+package tests;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,10 +14,10 @@ public class CheckCartTest {
     1. Продолжаем в проекте SauceDemo
     2. Закончить поиск локаторов после практики
     3. Создать отдельный Java-класс с тестом, сценарий:
-    a. Залогиниться
-    b. Добавить товар в корзину
-    c. Перейти в корзину
-    d. Проверить (assertEquals) стоимость товара и его имя в корзине
+    3a. Залогиниться
+    3b. Добавить товар в корзину
+    3c. Перейти в корзину
+    3d. Проверить (assertEquals) стоимость товара и его имя в корзине
     4. Создать Pull Request и добавить ментора в коллабораторы
      */
     @Test
@@ -31,26 +33,23 @@ public class CheckCartTest {
         options.addArguments("--disable-infobars");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.get("https://www.saucedemo.com/");
-
         //Логинимся на сайте
         driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
         driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-
+        //получаем наименование товара и цену в списке товаров
         String nameItem = driver.findElement(By.xpath("//*[@id=\"item_5_title_link\"]")).getText();
         String priceItem = driver.findElement(By.xpath("//*[@id='item_5_title_link']/ancestor::div[@class='inventory_item']//descendant::div[@class='inventory_item_price']")).getText();
-
+        //добавляем товар в корзину
         driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-fleece-jacket\"]")).click();
-
+        //переходим в корзину
         driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]")).click();
         String nameItemInCart = driver.findElement(By.xpath("//*[@id=\"item_5_title_link\"]")).getText();
         String priceItemInCart = driver.findElement(By.xpath("//*[@class='inventory_item_price']")).getText();
-
+        //сравниваем наименование товара и цену в списке и в корзине
         Assert.assertEquals(nameItemInCart, nameItem);
         Assert.assertEquals(priceItemInCart, priceItem);
-
         driver.quit();
     }
 }
--- src/test/java/tests/LocatorTest.java
+++ src/test/java/tests/LocatorTest.java
@@ -1,3 +1,4 @@
+package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,87 +21,62 @@ public class LocatorTest {
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-
         driver.findElement(By.id("user-name"));
         driver.findElement(By.name("user-name"));
         driver.findElement(By.className("login_credentials_wrap"));
         driver.findElement(By.tagName("div"));
-
         //логинимся
         driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
         driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-
+        //продолжаем поиск локаторов
         driver.findElement(By.linkText("Sauce Labs Backpack"));
         driver.findElement(By.partialLinkText("Bolt"));
-
         //xpath: поиск по атрибуту, например By.xpath("//tag[@attribute='value']");
         driver.findElement(By.xpath("//div[@class='app_logo']"));
-
         //xpath: поиск по тексту, например By.xpath("//tag[text()='text']");
         driver.findElement(By.xpath("//div[text()='Swag Labs']"));
-
         //xpath: поиск по частичному совпадению атрибута, например
         //By.xpath("//tag[contains(@attribute,'text')]");
         driver.findElement(By.xpath("//div[contains(@id,'page')]"));
-
         //xpath: поиск по частичному совпадению текста, например By.xpath("//tag[contains(text(),'text')]");
         driver.findElement(By.xpath("//div[contains(text(),'Swag')]"));
-
         //xpath: ancestor, например //*[text()='Enterprise Testing']//ancestor::div
         driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']//ancestor::div[@class='inventory_item_label']"));
-
         //xpath: descendant
         driver.findElement(By.xpath("//*[@id='inventory_container']//descendant::div[@class='inventory_item_img']"));
-
         //xpath: following
         driver.findElement(By.xpath("//div[@id='inventory_container']//following::div[@class='inventory_item']"));
-
         //xpath: parent
         driver.findElement(By.xpath("//div[@id='inventory_container']//parent::div[@id='contents_wrapper']"));
-
         //xpath: preceding
         driver.findElement(By.xpath("//div[@class='inventory_item_desc']//preceding::*[contains(text(), 'Sauce Labs Backpack')]"));
-
         //xpath: *поиск элемента с условием AND, например //input[@class='_2zrpKA_1dBPDZ'and @type='text']
         driver.findElement(By.xpath("//button[contains(@data-test, 'add-to-cart') and contains(@id, 'sauce-labs-backpack')] "));
-
         //css: .class
         driver.findElement(By.cssSelector(".bm-burger-button"));
-
         //css: .class1.class2
         driver.findElement(By.cssSelector(".btn.btn_primary.btn_small.btn_inventory"));
-
         //css: .class1 .class2
         driver.findElement(By.cssSelector(".shopping_cart_container .shopping_cart_link"));
-
         //css: #id
         driver.findElement(By.cssSelector("#item_4_title_link"));
-
         //css: tagname
         driver.findElement(By.cssSelector("button"));
-
         //css: tagname.class
         driver.findElement(By.cssSelector("select.product_sort_container"));
-
         //css: [attribute=value]
         driver.findElement(By.cssSelector("[class=pricebar]"));
-
         //css: [attribute~=value]
         driver.findElement(By.cssSelector("[class~=btn]"));
-
         //css: [attribute|=value]
         driver.findElement(By.cssSelector("[data-test|=secondary]"));
-
         //css: [attribute^=value]
         driver.findElement(By.cssSelector("[href^=https]"));
-
         //css: [attribute$=value]
         driver.findElement(By.cssSelector("[href$=\"saucelabs\"]"));
-
         //css: [attribute*=value]
         driver.findElement(By.cssSelector("[href*=\"sauce\"]"));
-
         driver.quit();
     }
 }
